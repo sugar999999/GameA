b0111c26d18e4d7832ff338ce9f4ad68689fa2bc
Yusuke Shiga <sugar_maestro@yahoo.co.jp>
2016-03-25 22:28:13 +0900

    Layout draft

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// main.js
+// by Sugar999999
+
+var main = (function(){
+
+  var initModule = function( $container ){
+    main.start.initModule($container);
+  };
+
+  return {initModule: initModule};
+
+}());
