03111325b4cbdc6b543267ea3f939f8fd7a0b3bb
Yusuke Shiga <sugar_maestro@yahoo.co.jp>
2016-03-29 16:01:38 +0900

    add stagelist and goal, but endless alert..

--- js/main.model.js
+++ js/main.model.js
@@ -18,25 +18,36 @@ main.model = (function(){
         + '<div id=\"output-map\"> << </div>'
       + '<\/div>',
 
-    stage_map:
-    [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+    stage: {
+      map:
+      [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+      start: {
+        col: 0,
+        row: 0
+      },
+      goal: {
+        col: 0,
+        row: 0
+      },
+      state: {}
+    },
 
-    block_size: 25,
+    block_size: 22,
 
     mouse: {
       X: 0,
@@ -48,11 +59,13 @@ main.model = (function(){
       swit: 0
     },
     disp_state: {
-      rad: 0
+      rad: 0,
+      wid: 352,
+      hei: 352
     },
     ball_state: {
-      X: this.radius,
-      Y: this.radius,
+      X: 0,
+      Y: 0,
       col: 0,
       row: 0,
       _col: -1,
@@ -60,7 +73,7 @@ main.model = (function(){
       gravity: 5,
       vx: 0,
       vy: 0,
-      radius: 10
+      radius: 8
     }
   },
   initModule, drawDisp, nav, onMouseMove, onMouseClick,
@@ -76,8 +89,18 @@ main.model = (function(){
     configMap.ball_state.col = Math.floor(configMap.ball_state.X / configMap.block_size);
     configMap.ball_state.row = Math.floor(configMap.ball_state.Y / configMap.block_size);
 
+    // ゴール判定
+    if( configMap.ball_state.col == configMap.stage.goal.col && configMap.ball_state.row == configMap.stage.goal.row ){
+      //goal();
+      clearinterval(configMap.stage.state.running);
+      alert("goal!");
+      return false;
+    }
+
     // ボールの位置更新
     // 下向きの重力に合わせてsin,cosを設定。（ball_state.X: gravity（下向き） の sin, ball_state.Y: gravity（下向き） の cos）
+    configMap.ball_state.vx = configMap.ball_state.X;
+    configMap.ball_state.vy = configMap.ball_state.Y;
     configMap.ball_state.vx += Math.sin(configMap.disp_state.rad * Math.PI / 180) * configMap.ball_state.gravity;
     configMap.ball_state.vy += Math.cos(configMap.disp_state.rad * Math.PI / 180) * configMap.ball_state.gravity;
     // 「mainCanvas」からスケールアウトしないように値を調整。
@@ -97,30 +120,30 @@ main.model = (function(){
       // Canvasの枠に接する場合、「configMap.ball_state._row」が定義外のインデックスを指さないように調整
       if( 0 > configMap.ball_state._row )configMap.ball_state._row = 0;
       if( 0 > configMap.ball_state._col )configMap.ball_state._col = 0;
-      if( configMap.ball_state._col >= configMap.stage_map[0].length )configMap.ball_state._col = configMap.stage_map[0].length -1;
-      if(configMap.ball_state._row >= configMap.stage_map.length)configMap.ball_state._row = configMap.stage_map.length -1;
+      if( configMap.ball_state._col >= configMap.stage.map[0].length )configMap.ball_state._col = configMap.stage.map[0].length -1;
+      if(configMap.ball_state._row >= configMap.stage.map.length)configMap.ball_state._row = configMap.stage.map.length -1;
 
       // ボール輪郭にブロックがあるかどうか判定。ブロック4面のどの面からの接触かによって場合分け。
       if(configMap.ball_state._col > configMap.ball_state.col){
-        if(configMap.stage_map[configMap.ball_state.row][configMap.ball_state._col] == 1){
+        if(configMap.stage.map[configMap.ball_state.row][configMap.ball_state._col] == 1){
           configMap.ball_state.vx = configMap.ball_state._col * configMap.block_size;
           configMap.ball_state.vx -= (Math.sin(i * Math.PI / 180) * configMap.ball_state.radius);
         }
       }
       if(configMap.ball_state._col < configMap.ball_state.col){
-        if(configMap.stage_map[configMap.ball_state.row][configMap.ball_state._col] == 1){
+        if(configMap.stage.map[configMap.ball_state.row][configMap.ball_state._col] == 1){
           configMap.ball_state.vx = configMap.ball_state._col * configMap.block_size + configMap.block_size;
           configMap.ball_state.vx -= (Math.sin(i * Math.PI / 180) * configMap.ball_state.radius);
         }
       }
       if(configMap.ball_state._row > configMap.ball_state.row){
-        if(configMap.stage_map[configMap.ball_state._row][configMap.ball_state.col] == 1){
+        if(configMap.stage.map[configMap.ball_state._row][configMap.ball_state.col] == 1){
           configMap.ball_state.vy = configMap.ball_state._row * configMap.block_size;
           configMap.ball_state.vy -= (Math.cos(i * Math.PI / 180) * configMap.ball_state.radius);
         }
       }
       if(configMap.ball_state._row < configMap.ball_state.row){
-        if(configMap.stage_map[configMap.ball_state._row][configMap.ball_state.col] == 1){
+        if(configMap.stage.map[configMap.ball_state._row][configMap.ball_state.col] == 1){
           configMap.ball_state.vy = configMap.ball_state._row * configMap.block_size + configMap.block_size;
           configMap.ball_state.vy -= (Math.cos(i * Math.PI / 180) * configMap.ball_state.radius);
         }
@@ -130,7 +153,6 @@ main.model = (function(){
     // ボールの位置を移動
     configMap.ball_state.X = configMap.ball_state.vx;
     configMap.ball_state.Y = configMap.ball_state.vy;
-
 
   };
 
@@ -143,7 +165,7 @@ main.model = (function(){
     var onOutputMap;
 
     // -----------onOutputMap------------start
-    // 「configMap.stage_map」を配列表示で出力する。
+    // 「configMap.stage.map」を配列表示で出力する。
     //
     onOutputMap = function(e){
       var $output =
@@ -151,10 +173,10 @@ main.model = (function(){
         .parent()
         .find("textarea");
 
-      for(var i=0; i < configMap.stage_map.length; i++){
+      for(var i=0; i < configMap.stage.map.length; i++){
         if(i === 0)$output.text('    [');
-        $output.append('[' + configMap.stage_map[i] + ']');
-        if(i != configMap.stage_map.length - 1){
+        $output.append('[' + configMap.stage.map[i] + ']');
+        if(i != configMap.stage.map.length - 1){
           $output.append(',\n    ');
           continue;
         }
@@ -176,6 +198,8 @@ main.model = (function(){
   // ------------------------drawDisp--------------------------start
   //
   drawDisp = function(){
+
+    var reset;
     // ---------------canvas px adapt------------------start
     style = window.getComputedStyle(mainCanvas);
     mainCanvas.width = +style.width.replace(/px/, "");
@@ -183,11 +207,17 @@ main.model = (function(){
     // ---------------canvas px adapt------------------end
     mainCont = mainCanvas.getContext("2d");
 
-    // draw stage_map
+    // 初回のみの演出
+    // ボールの描画
+    reset = function(){
+
+    };
+
+    // draw stage.map
     mainCont.fillStyle = "#aaaaaa";
-    for(var i=0; i < configMap.stage_map.length; i++){
-      for(var j=0; j < configMap.stage_map[i].length; j++){
-        if(configMap.stage_map[i][j] == 1)mainCont.fillRect(j * configMap.block_size, i * configMap.block_size, configMap.block_size, configMap.block_size);
+    for(var i=0; i < configMap.stage.map.length; i++){
+      for(var j=0; j < configMap.stage.map[i].length; j++){
+        if(configMap.stage.map[i][j] == 1)mainCont.fillRect(j * configMap.block_size, i * configMap.block_size, configMap.block_size, configMap.block_size);
       }
     }
 
@@ -196,6 +226,12 @@ main.model = (function(){
     mainCont.fillStyle = "#888888";
     mainCont.beginPath();
     mainCont.arc(configMap.ball_state.X, configMap.ball_state.Y, configMap.ball_state.radius, 0, Math.PI*2, true);
+    mainCont.fill();
+
+    // draw goal
+    mainCont.fillStyle = "#d00";
+    mainCont.beginPath();
+    mainCont.arc((configMap.stage.goal.col * configMap.block_size) + configMap.block_size / 2, (configMap.stage.goal.row * configMap.block_size) + configMap.block_size / 2, configMap.ball_state.radius, 0, Math.PI*2, true);
     mainCont.fill();
   };
 
@@ -222,7 +258,7 @@ main.model = (function(){
     configMap.mouse.col = Math.floor(configMap.mouse.X / configMap.block_size);
     configMap.mouse.row = Math.floor(configMap.mouse.Y / configMap.block_size);
 
-    if( (configMap.mouse.col < configMap.stage_map[0].length) && (configMap.mouse.row < configMap.stage_map.length) ){
+    if( (configMap.mouse.col < configMap.stage.map[0].length) && (configMap.mouse.row < configMap.stage.map.length) ){
 
       // 取得した現在位置ブロックの状態に応じて、クリック時の動作を判定する。
       // 空白：ブロックを埋める。
@@ -230,8 +266,8 @@ main.model = (function(){
       // 例外：クリックされたまま他のブロックに移った場合、最初のブロック状態の判定から変更しない。
       //  * configMap.mouse.swit：クリック時の動作。「0」消す。「1」埋める。
       //
-      if(e.buttons === 0 && configMap.stage_map[configMap.mouse.row][configMap.mouse.col] === 0)configMap.mouse.swit = 1;
-      else if(e.buttons === 0 && configMap.stage_map[configMap.mouse.row][configMap.mouse.col] == 1)configMap.mouse.swit = 0;
+      if(e.buttons === 0 && configMap.stage.map[configMap.mouse.row][configMap.mouse.col] === 0)configMap.mouse.swit = 1;
+      else if(e.buttons === 0 && configMap.stage.map[configMap.mouse.row][configMap.mouse.col] == 1)configMap.mouse.swit = 0;
 
       // クリック時の動作
       //「configMap.mouse.swit」を現在位置ブロックに代入する。
@@ -239,7 +275,7 @@ main.model = (function(){
       //  * configMap.mouse._Col, configMap.mouse._Row：前回動作した位置ブロックの保存。
       //    クリックにて押下されたボタンが放れれば、数値がリセットされる。→同じ位置で移動せず再度クリックしても正常動作。
       if(e.buttons == 1 && !(configMap.mouse._Col == configMap.mouse.col && configMap.mouse._Row == configMap.mouse.row) ){
-        configMap.stage_map[configMap.mouse.row][configMap.mouse.col] = configMap.mouse.swit;
+        configMap.stage.map[configMap.mouse.row][configMap.mouse.col] = configMap.mouse.swit;
         drawDisp();
         configMap.mouse._Col = configMap.mouse.col;
         configMap.mouse._Row = configMap.mouse.row;
@@ -256,8 +292,8 @@ main.model = (function(){
   // --------------------onMouseClickCanvas----------------------start
   //
   onMouseClickCanvas = function(e){
-    if(configMap.stage_map[configMap.mouse.row][configMap.mouse.col] === 0)configMap.stage_map[configMap.mouse.row][configMap.mouse.col] = 1;
-    else configMap.stage_map[configMap.mouse.row][configMap.mouse.col] = 0;
+    if(configMap.stage.map[configMap.mouse.row][configMap.mouse.col] === 0)configMap.stage.map[configMap.mouse.row][configMap.mouse.col] = 1;
+    else configMap.stage.map[configMap.mouse.row][configMap.mouse.col] = 0;
     drawDisp();
   };
   //
@@ -278,15 +314,19 @@ main.model = (function(){
         .animate({width: 100 + "%"}, 1000)
         .animate({height: 500 + "px"}, 1000)
       .find("#main-disp-window") //Window
-        .animate({width: 400 + "px"}, 1000)
-        .animate({height: 400 + "px" }, 1000)
+        .css("margin", (-configMap.disp_state.wid/2) + " 0 0 " + (-configMap.disp_state.wid/2) )
+        .animate({width: configMap.disp_state.wid + "px"}, 1000)
+        .animate({height: configMap.disp_state.hei + "px" }, 1000)
       .parent().next() //Footer
         .animate({width: 100 + "%"}, 1000)
         .css("bottom","0")
         .animate({height: 100 + "px" }, {
           duration: "1000",
           complete: function(){
-            setInterval(drawDisp, 30);
+            main.stagelist.mapMake(configMap.stage, "_1");
+            configMap.ball_state.X = (configMap.stage.start.col * configMap.block_size) + configMap.ball_state.radius;
+            configMap.ball_state.Y = (configMap.stage.start.row * configMap.block_size) + configMap.ball_state.radius;
+            configMap.stage.state.running = setInterval(drawDisp, 30);
           }
         });
 
